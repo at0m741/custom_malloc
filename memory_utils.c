71719d825c6f88a408e7bd59ee066f7488d67f7c
at0m741 <louistouz@icloud.com>
2024-10-16 03:02:47 +0200

    Memcpy_ERMS

--- memory_utils.c
+++ memory_utils.c
@@ -1,7 +1,10 @@
 #include "include.h"
 
-void *_memcpy_ERMS(void *dest, const void *src, size_t n) {
-	__asm__ __volatile__ (
+__attribute__((always_inline, hot))
+inline void *_memcpy_ERMS(void *dest, const void *src, size_t n) 
+{
+	__asm__ __volatile__ 
+    (
 		"rep movsb"
 		: "+D"(dest), "+S"(src), "+c"(n)
 		:
